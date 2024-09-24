3190174c2eb59302415ec64c4a4a764a433ea37d
imanollramirez <aleimanolramirez@gmail.com>
2024-09-24 07:47:18 -0600

    initial commit

---
+++ src/soldadores/app/SoldadoresApp.java
@@ -0,0 +1,12 @@
+package soldadores.app;
+
+public class SoldadoresApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
